66fbd1fd8d0fa42eac54f7d2cefcfa2e6187087a
eliaszayoud-SE <eng.eliaszayoud@gmail.com>
2024-08-05 00:59:05 +0300

    fix

--- store/notification.py
+++ store/notification.py
@@ -15,25 +15,27 @@ def _get_access_token():
   
   service_account_info = {
   "type": "service_account",
-  "project_id": os.environ.get('PROJECT_ID'),
-  "private_key_id": os.environ.get('PRIVATE_KEY_ID'),
-  "private_key": os.environ.get('PRIVATE_KEY'),
-  "client_email": os.environ.get('CLIENT_EMAIL'),
-  "client_id": os.environ.get('CLIENT_ID'),
+  "project_id": f"{os.environ.get('PROJECT_ID')}",
+  "private_key_id": f"{os.environ.get('PRIVATE_KEY_ID')}",
+  "private_key": f"{os.environ.get('PRIVATE_KEY')}",
+  "client_email": f"{os.environ.get('CLIENT_EMAIL')}",
+  "client_id": f"{os.environ.get('CLIENT_ID')}",
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
   "token_uri": "https://oauth2.googleapis.com/token",
   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
   "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-ypjlm%40ecommerce-31838.iam.gserviceaccount.com",
   "universe_domain": "googleapis.com"
-} 
+}
   
 
 
   print('1')
+  
   try :
-      credentials = service_account.Credentials.from_service_account_info(service_account_info, scopes=SCOPES)
+    credentials = service_account.Credentials.from_service_account_info(service_account_info, scopes=SCOPES)
   except Exception as error:
-    print("An error occurred:", error)
+    print(error)
+    
   print('2')
   request = google.auth.transport.requests.Request()
   print('3')
@@ -67,7 +69,7 @@ def send_notification(title, message, topic, pageid, pagename):
 
   print('6')
 
-  conn.request("POST", f"/v1/projects/{os.environ.get('PROJECT_ID')}/messages:send", payload, headersList)
+  conn.request("POST", f"/v1/projects/ecommerce-31838/messages:send", payload, headersList)
   print('7')
   response = conn.getresponse()
   print('8')

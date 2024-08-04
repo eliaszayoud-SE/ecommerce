f85d671844262eac0cc55b6e0176d7486d0024d4
eliaszayoud-SE <eng.eliaszayoud@gmail.com>
2024-08-04 15:45:52 +0300

    try to fix error

--- store/crypto.py
+++ store/crypto.py
@@ -1,12 +1,21 @@
 import os
 from cryptography.fernet import Fernet
+from pathlib import Path
+
+
 
 key = os.environ.get("FERNET_KEY")
 
 fernet = Fernet(key)
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+file_path = BASE_DIR + '/store/ecommerce-service.json'
+
+print(file_path)
+
 def decrypt():
-    with open('ecommerce-service.json', 'rb') as enc_file:
+    with open(file_path, 'rb') as enc_file:
         encrypted = enc_file.read()
  
     # decrypting the file
@@ -14,7 +23,7 @@ def decrypt():
     
     # opening the file in write mode and
     # writing the decrypted data
-    with open('ecommerce-service.json', 'wb') as dec_file:
+    with open(file_path, 'wb') as dec_file:
         dec_file.write(decrypted)
 
 decrypt()
